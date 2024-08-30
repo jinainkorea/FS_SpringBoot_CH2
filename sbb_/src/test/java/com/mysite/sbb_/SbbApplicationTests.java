24bdadb062742d65403eedb10faa7175f6c11a4d
jinainkorea <kimjina1152@gmail.com>
2024-08-30 10:24:43 +0900

    init

---
+++ sbb_/src/test/java/com/mysite/sbb_/SbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
