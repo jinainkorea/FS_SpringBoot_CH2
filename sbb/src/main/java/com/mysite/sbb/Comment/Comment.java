055201327df87a5996f1cbe330f8501d3be168fe
jinainkorea <kimjina1152@gmail.com>
2024-09-02 11:47:02 +0900

    Connect DB & Create Entity

--- sbb/src/main/java/com/mysite/sbb/Comment/Comment.java
+++ sbb/src/main/java/com/mysite/sbb/Comment/Comment.java
@@ -1,5 +1,22 @@
 package com.mysite.sbb.Comment;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
 public class Comment {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
 
 }
