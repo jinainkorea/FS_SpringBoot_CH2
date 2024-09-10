efc0cdde77e583d37ad6516cec8f5a241ea79d01
jinainkorea <kimjina1152@gmail.com>
2024-09-10 09:30:33 +0900

    init

---
+++ src/main/java/com/mysite/sbb2_5/Sbb25Application.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb2_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbb25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbb25Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/sbb2_5/Sbb25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb2_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
