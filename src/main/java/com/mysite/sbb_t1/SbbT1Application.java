a6402070b30571f097ae8081054641722f94a1cf
jinainkorea <kimjina1152@gmail.com>
2024-09-11 09:24:40 +0900

    init

---
+++ src/main/java/com/mysite/sbb_t1/SbbT1Application.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb_t1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbT1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbbT1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/sbb_t1/SbbT1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb_t1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbT1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
