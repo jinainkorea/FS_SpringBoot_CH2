aff5d05b0e2617d8ff9fc2bb2b6daa162ae952ef
jinainkorea <kimjina1152@gmail.com>
2024-09-11 10:13:23 +0900

    페이지 디자인, '글 등록하기'로 (/article/create)등록 폼 이동

--- src/main/java/com/mysite/sbb_t1/Article/ArticleController.java
+++ src/main/java/com/mysite/sbb_t1/Article/ArticleController.java
@@ -1,9 +1,12 @@
 package com.mysite.sbb_t1.Article;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,5 +23,10 @@ public class ArticleController {
     List<Article> articleList = this.articleService.getList();
     model.addAttribute("articleList", articleList);
     return "article_list";
+  }
+
+  @GetMapping("/create")
+  public String create(ArticleForm articleForm) {
+    return "article_form";
   }
 }
--- src/main/java/com/mysite/sbb_t1/Article/ArticleService.java
+++ src/main/java/com/mysite/sbb_t1/Article/ArticleService.java
@@ -3,6 +3,7 @@ package com.mysite.sbb_t1.Article;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
