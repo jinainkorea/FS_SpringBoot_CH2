7f04d7fa4ffaf0366029ff4fbf123fe99809c471
jinainkorea <kimjina1152@gmail.com>
2024-09-11 10:28:27 +0900

    리스트 페이지에서 상세페이지 이동

--- src/main/java/com/mysite/sbb_t1/Article/ArticleController.java
+++ src/main/java/com/mysite/sbb_t1/Article/ArticleController.java
@@ -5,10 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,5 +31,12 @@ public class ArticleController {
   public String createArticle(ArticleForm articleForm) {
     this.articleService.create(articleForm.getTitle(), articleForm.getContent());
     return "redirect:/";
+  }
+
+  @GetMapping("/detail/{id}")
+  public String detail(Model model, @PathVariable("id") Integer id) {
+    Article article = this.articleService.getArticleById(id).get();
+    model.addAttribute("article", article);
+    return "article_detail";
   }
 }
--- src/main/java/com/mysite/sbb_t1/Article/ArticleService.java
+++ src/main/java/com/mysite/sbb_t1/Article/ArticleService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,5 +22,9 @@ public class ArticleService {
     article.setContent(content);
     article.setCreateDate(LocalDateTime.now());
     articleRepository.save(article);
+  }
+
+  public Optional<Article> getArticleById(Integer id) {
+    return this.articleRepository.findById(id);
   }
 }
